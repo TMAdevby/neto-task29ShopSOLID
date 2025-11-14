acada8508ac90879f78059ad1904c5669998ff59
TMAdevby <mtipun@mail.ru>
2025-11-14 20:51:21 +0300

    solid2

---
+++ src/main/java/shop/repository/OrderRepository.java
@@ -0,0 +1,24 @@
+package shop.repository;
+
+import shop.model.Order;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class OrderRepository {
+    private final Map<String, Order> orders = new HashMap<>();
+
+    public void save(Order order) {
+        orders.put(order.getOrderId(), order);
+    }
+
+    public Order findById(String id) {
+        return orders.get(id);
+    }
+
+    public List<Order> findAll() {
+        return new ArrayList<>(orders.values());
+    }
+}
